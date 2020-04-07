a4ab8ecda0f9c5173586e44c6bc1ea0f3c45399d
Zijian Wang <Jeff@Zijians-MacBook-Air.local>
2020-04-06 20:00:01 -0400

    fix error of line 518

--- virusviz18.py
+++ virusviz18.py
@@ -516,28 +516,28 @@ class runVirusViz(object):
     # refer to https://github.com/HCui91/covid-19-model	    	
 	#   https://zhuanlan.zhihu.com/p/104645873
     def SIR(self, t, beta, gamma):
-	    # Total population, N.
-	    N = 1000000
-	    # Initial number of infected and recovered individuals, I0 and R0.
-	    I0, R0 = 65, 65.0/239.0*25.0
-	    # Everyone else, S0, is susceptible to infection initially.
-	    S0 = N - I0 - R0
+        # Total population, N.
+        N = 1000000
+        # Initial number of infected and recovered individuals, I0 and R0.
+        I0, R0 = 65, 65.0/239.0*25.0
+        # Everyone else, S0, is susceptible to infection initially.
+        S0 = N - I0 - R0
 
-	    # The SIR model differential equations.
-	    # @njit
-	    def deriv(y, t, N, beta, gamma):
-		S, I, R = y
-		dSdt = -beta * S * I / N
-		dIdt = beta * S * I / N - gamma * I
-		dRdt = gamma * I
-		return dSdt, dIdt, dRdt
+        # The SIR model differential equations.
+        # @njit
+        def deriv(y, t, N, beta, gamma):
+            S, I, R = y
+            dSdt = -beta * S * I / N
+            dIdt = beta * S * I / N - gamma * I
+            dRdt = gamma * I
+            return dSdt, dIdt, dRdt
 
-	    # Initial conditions vector
-	    y0 = S0, I0, R0
-	    # Integrate the SIR equations over the time grid, t.
-	    ret = odeint(deriv, y0, t, args=(N, beta, gamma))
-	    S, I, R = ret.T
-	    return I
+        # Initial conditions vector
+        y0 = S0, I0, R0
+        # Integrate the SIR equations over the time grid, t.
+        ret = odeint(deriv, y0, t, args=(N, beta, gamma))
+        S, I, R = ret.T
+        return I
 
     #
     def predictByModelSir(self, type_data=0):
