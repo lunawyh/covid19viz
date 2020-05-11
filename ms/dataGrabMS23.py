f5366f5820e321332a60c141bad20860a175b938
luna <lunawyh@gmail.com>
2020-05-11 18:18:09 -0400

    up load

--- ms/dataGrabMS23.py
+++ ms/dataGrabMS23.py
@@ -45,7 +45,7 @@ class dataGrabMS(object):
         l_overall.append(['County', 'Cases', 'Deaths'])
         for a_item in l_raw_data:
             # remove ******************************************************
-            a_item[2] = str(a_item[2]).replace('*', '')
+            a_item[1] = (a_item[1]).replace('*', '')
             #***************************************
             #print(a_item)
             l_overall.append(a_item[:3])
