e170a4a656f9de1d8317e5114ccc0c8390c8ff79
jeff <zijian.wang@fcagroup.com>
2020-06-13 17:48:13 -0400

    update codes for MI and CA

--- ca/dataGrab58.py
+++ ca/dataGrab58.py
@@ -22,6 +22,12 @@ from os.path import isfile, join
 from matplotlib.patches import Wedge
 import matplotlib.pyplot as plt
 import math
+
+####New imports
+import urllib2
+import numpy
+from numpy import savetxt
+import datetime
 # ==============================================================================
 # -- codes -------------------------------------------------------------------
 # ==============================================================================
@@ -61,19 +67,81 @@ class dataGrab(object):
             l_data = self.parseDfData(df)
         else: return []
         return l_data
+    ## save to csv 
+    def save2File(self, l_data, csv_name):
+        csv_data_f = open(csv_name, 'w')
+        # create the csv writer 
+        csvwriter = csv.writer(csv_data_f)
+        # make sure the 1st row is colum names
+        if('County' in str(l_data[0][0])): pass
+        else: csvwriter.writerow(['County', 'Cases', 'Deaths'])
+        for a_row in l_data:
+            csvwriter.writerow(a_row)
+        csv_data_f.close()
+    ## open a csv 
+    def saveDayData(self, l_day_data):
+        print('  saveDayData')
+        date=l_day_data[-1,1]
+        dt_name=datetime.datetime.strptime(date, '%m/%d/%Y').strftime('%Y%m%d')
+        dt_now=datetime.datetime.strptime(date, '%m/%d/%Y').strftime('%m/%d/%Y')  # use same format
+        print('  date on', date)
+        originalData = l_day_data.T
+        #Create the data
+        lst_data=[]
+        lst_data.append(['County', 'Cases', 'Deaths', 'Date'])
+        lst_data.extend(originalData[[0, 2, 3, 1]].T)
+        total_daily=(sum(map(int, originalData[2])))
+        total_death=(sum(map(int, originalData[3])))
+        lst_data.append(['Total', total_daily, total_death, 'Today'])
+        # save
+        f_name = self.state_dir + 'data/'+self.state_name.lower()+'_covid19_'+dt_name+'.csv'
+        self.save2File( lst_data, f_name )
+        return (lst_data, dt_name, dt_now)
     ## paser data CA
     def parseDataCa(self, name_file):
+        url = self.l_state_config[5][1]
+        print('  grab data from', url)
+        response = urllib2.urlopen(url)
+        cr = csv.reader(response)
+        
+        xx = list(cr)
+        arr_data_all = numpy.array(xx)
+        # get kinds of date
+        l_dates = sorted( list(set(arr_data_all[1:].T[1])) )
+        print('  dates', l_dates)
+        #l_day_data = []
+        for a_date in l_dates:
+            if('4/1/2020' == a_date): break
+            l_day_data = []
+            for a_item in arr_data_all:
+                if(a_date == a_item[1]): l_day_data.append(a_item)
+            (lst_data, dt_name, dt_now) = self.saveDayData( numpy.array(l_day_data) )
+
+        #Save the raw data
+        link_dir =  self.state_dir + 'data_raw/'
+        if(not os.path.isdir(link_dir) ): os.mkdir(link_dir)
+        fn_raw = self.state_name.lower()+'_covid19_'+dt_name+'.csv'
+        urllib.urlretrieve(self.l_state_config[5][1], link_dir+fn_raw)  #, context=gcontext)
+        print('  save raw data to', link_dir+fn_raw)
+        #filename="data/ca_covid19_"+dd+".csv"
+        #numpy.savetxt(link_dir+fn_raw, x, fmt='"%s"')
+        #print(filename)
+        return (lst_data, dt_name, dt_now)
+
+    ## paser data of Counties
+    def parseDataCaCounties(self, name_file):
         l_links = self.open4File(self.state_dir + self.l_state_config[5][1])
         if(not os.path.isdir(self.state_dir + 'data_html/') ): os.mkdir(self.state_dir + 'data_html/')
         link_dir =  self.state_dir + 'data_html/' + name_file + '/'
         if(not os.path.isdir(link_dir) ): os.mkdir(link_dir)
-        
+        print('  grab html by', len(l_links) )       
         for a_link in l_links:
                 f_name = link_dir+a_link[0]+'.html'
                 #print(a_link[1], f_name)
                 #gcontext = ssl._create_unverified_context()
                 urllib.urlretrieve(a_link[1], f_name)  #, context=gcontext)
         
+        print('  parse data by', len(l_links) )       
         l_data_daily = []
         total_daily = 0
         total_death = 0
