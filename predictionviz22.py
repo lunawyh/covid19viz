b8d22e21810cd9e812a9f1f598bd77112a35f698
Zijian Wang <jeffwang@Zijians-Mac-mini.local>
2020-06-20 17:17:20 -0400

    update codes prediction for TX

--- predictionviz22.py
+++ predictionviz22.py
@@ -142,16 +142,17 @@ class predictionViz(object):
             for a_day in l_data_day:
                 #print(a_day[0])                
                 if 'Total' in str(a_day[0]) or '56' in str(a_day[0]):
-                    lst_data_overall = a_day[11:]
-
                     print('  Total is read', a_day[0:3])
+                    lst_data_overall = a_day[11:]
                     #    lst_data_overall.append(int(a_number))
+                    break
                 if 'County Name' in str(a_day[0]) or 'Cases' in str(a_day[0]):
-                    #print(')))))))))))))))))))))))))))))))', a_day[11:])
-                    for a_date in a_day[11:]:
-                        day_mmdd.append(a_date.replace('Cases\n', ''))
                     print('  Date is read', a_day[0:3])
+                    for a_date in a_day[11:]:
+                        #print('      ', a_date)
+                        day_mmdd.append(a_date.replace('Cases', '').replace('\r', '').replace('\n', '').replace(' ', ''))
                     #day_mmdd = a_day[0:]
+                    
             if( len(day_mmdd) < 1): return False
             #print(')))))))))))))))))))))))))))))))', day_mmdd)
             dt_s = datetime.datetime.strptime('2020-'+day_mmdd[-1], '%Y-%m-%d')         
