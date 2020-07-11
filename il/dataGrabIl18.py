cffd4ace4fe91cd4a0919eb1b73d6c3438510b09
jeff <zijian.wang@fcagroup.com>
2020-07-11 11:29:44 -0400

    add IN codes

--- il/dataGrabIl18.py
+++ il/dataGrabIl18.py
@@ -240,7 +240,7 @@ class dataGrabIl(object):
             return np.vstack((l_data.T, np.array(['Total', total_cases, total_death])))
         return []
     ## paser data CA
-    def parseData(self, name_target, type_download):
+    def parseData(self, name_target, date_target, type_download):
             self.name_file = name_target
             f_name = self.state_dir + 'data_html/'+self.state_name.lower()+'_covid19_'+self.name_file+'.html'
             if(not os.path.isdir(self.state_dir + 'data_html/') ): os.mkdir(self.state_dir + 'data_html/')
