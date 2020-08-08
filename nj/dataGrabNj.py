e8adba80f270f19364e479ece0c8b2ab4b51909a
unknown <dyang42069@gmail.com>
2020-08-08 16:36:29 -0400

    update data

--- nj/dataGrabNj.py
+++ nj/dataGrabNj.py
@@ -44,118 +44,6 @@ class dataGrabNj(object):
         self.name_file = ''
         self.now_date = ''
     ## save to csv
-    def downloadFileMa(self, link_name):
-        urllib.urlopen(link_name)
-
-    def save2File(self, l_data, csv_name):
-        csv_data_f = open(csv_name, 'wb')
-        # create the csv writer
-        csvwriter = csv.writer(csv_data_f)
-        # make sure the 1st row is colum names
-        if('County' in str(l_data[0][0])): pass
-        else: csvwriter.writerow(['County', 'Cases', 'Deaths'])
-        for a_row in l_data:
-            csvwriter.writerow(a_row)
-        csv_data_f.close()
-    ## parse from exel format to list
-    def parseDfData(self, df, fName=None):
-        (n_rows, n_columns) = df.shape
-        # check shape
-        # print('parseDfData', df.title)
-        lst_data = []
-        for ii in range(n_rows):
-            a_case = []
-            for jj in range(n_columns):
-                if (str(df.iloc[ii, jj]) == 'nan'):
-                    a_case.append(0)
-                    continue
-                a_case.append(df.iloc[ii, jj])
-            lst_data.append(a_case)
-        # save to a database file
-        if (fName is not None): self.save2File(lst_data, fName)
-        return lst_data
-
-    ## open a csv
-    def open4File(self, csv_name):
-        if (isfile(csv_name)):
-            df = pd.read_csv(csv_name)
-            l_data = self.parseDfData(df)
-        else:
-            return []
-        return l_data
-    ## open a csv
-    def open4FileBuffer(self, csv_data):
-        lst_data = []
-        reader = csv.reader(csv_data)
-        for row in reader:
-            lst_data.append(row)
-        return lst_data
-
-    ## save to csv
-    def saveLatestDateMa(self, l_data):
-        print('  saveLatestDateMa ...')
-        l_d_sort = sorted(l_data, key=lambda k: k[0], reverse=False)
-        # find different date time
-        l_date = []
-        for a_item in l_d_sort:
-            if('/' in a_item[0]): pass
-            else: continue
-            bFound = False
-            for a_date in l_date:
-                if(a_date in a_item[0]):
-                    bFound = True
-                    break
-            if(not bFound):
-                l_date.append(a_item[0])
-        print('  data in days', len(l_date) )
-        # generate all daily data
-        l_daily_latest = []
-        max_name_file = '20200101'
-        for a_date in l_date:
-            l_daily, n_name_file  = self.saveDataFromDlMa(l_d_sort, a_date, bDaily=False)
-            if(n_name_file > max_name_file): 
-                max_name_file = n_name_file
-                l_daily_latest = l_daily
-                dt_obj = datetime.datetime.strptime(n_name_file, '%Y%m%d')
-                self.name_file = dt_obj.strftime('%Y%m%d')
-                self.now_date = dt_obj.strftime('%m/%d/%Y')
-        return l_daily_latest
-
-    def saveDataFromDlMa(self, l_data, a_test_date, bDaily=True):
-        initial_test_date = None
-        #
-        l_overral = []
-        #
-        total_overral = 0
-        total_overral_deaths = 0
-        n_name_file = '20200101'
-        for a_item in l_data:
-            #if (a_test_date is None):
-            if (initial_test_date is None and a_test_date in a_item[0]):
-                initial_test_date = a_test_date
-                dt_obj = datetime.datetime.strptime(a_test_date, '%m/%d/%Y')
-                n_name_file = dt_obj.strftime('%Y%m%d')
-                
-            elif (a_test_date in a_item[0]):
-                pass
-            else:
-                continue
-            if('' == a_item[2]): a_item[2] = 0
-            if('' == a_item[3]): a_item[3] = 0
-            total_overral += int(a_item[2])
-            total_overral_deaths += int(a_item[3])
-            #
-            l_overral.append([a_item[1], a_item[2], a_item[3]])
-        l_overral.sort(key=lambda county: county[0])
-        #
-        l_overral.append(['Total', total_overral, total_overral_deaths])
-        #
-        if (not os.path.isdir(self.state_dir + 'data/')): os.mkdir(self.state_dir + 'data/')
-        #
-        self.save2File(l_overral,
-                       self.state_dir + 'data/' + self.state_name.lower() + '_covid19_' + n_name_file + '.csv')
-
-        return l_overral, n_name_file
 
     def downloadAndParseLink(self,link_address,fRaw):
         siteOpen = webdriver.Chrome()
@@ -177,7 +65,7 @@ class dataGrabNj(object):
                 if w == "County":
                     break
                 else:
-                    countyList = str(countyList + " " + str(w))
+                    countyList = str(countyList + str(w))
             del dStringList[0:(dStringList.index('County')+1)]
             allList.append([countyList,int(str(dStringList[3]).replace(',','')),int(str(dStringList[6]).replace(',',''))])
             totalPositives = totalPositives + int(str(dStringList[3]).replace(',',''))
@@ -198,6 +86,11 @@ class dataGrabNj(object):
                 wr.writerow(c)
             myfile.close()
         print('  saved to', sRaw)
+        with open('C:/Dennis/Covid19/covid19viz/nj/debugging/nj.txt','wb') as filemy:
+            filemy.write(str(nj_info))
+            filemy.write(str(type(nj_info)))
+            filemy.close()
+        return nj_info
 
     ## paser data CA
     def parseData(self, name_target, date_target, type_download):
@@ -207,6 +100,7 @@ class dataGrabNj(object):
             if(not os.path.isdir(self.state_dir + 'data_raw/') ): os.mkdir(self.state_dir + 'data_raw/')
             # step A: downlowd and save
             data_csv = self.saveData(f_name, s_name)
+            print(len(data_csv))
             return(data_csv, self.name_file, self.now_date)
 
 ## end of file
