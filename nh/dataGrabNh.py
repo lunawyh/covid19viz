d061bc3036d22e34a2210ba1f801ea94dcd811ee
dennis-yang-D <dyang42069@gmail.com>
2020-10-10 20:30:48 -0400

    nh

--- nh/dataGrabNh.py
+++ nh/dataGrabNh.py
@@ -27,18 +27,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import PyPDF2
-<<<<<<< HEAD
+
 import pytesseract
 from PIL import Image, ImageEnhance
 import cv2
 from pdfminer.high_level import extract_text
 
-=======
+
 #from pdfminer.high_level import extract_text
 import numpy as np
 import pytesseract 
 import cv2
->>>>>>> a05e568f17e0b8f0ecef36412d2e479fe5d10eaa
+
 # ==============================================================================
 # -- codes -------------------------------------------------------------------
 # ==============================================================================
@@ -55,48 +55,15 @@ class dataGrabNh(object):
         self.l_state_config = l_config
         self.name_file = ''
         self.now_date = ''
-    ## save to csv 
-    def save2File(self, l_data, csv_name):
-        csv_data_f = open(csv_name, 'w')
-        # create the csv writer 
-        csvwriter = csv.writer(csv_data_f)
-        # make sure the 1st row is colum names
-        if('County' in str(l_data[0][0])): pass
-        else: csvwriter.writerow(['County', 'Cases', 'Deaths'])
-        for a_row in l_data:
-            csvwriter.writerow(a_row)
-        csv_data_f.close()
-        print('  save2File', csv_name)
-    ## save downloaded data to daily or overal data 
-    def saveLatestDate(self, l_raw_data, fname):
-        l_overall = []
-        
-        l_overall.append(['County', 'Cases', 'Deaths'])
-        total_case = 0
-        total_death = 0
-        for a_item in l_raw_data:
-            l_overall.append(a_item)
-            total_case += int(a_item[1])
-            total_death += int(a_item[2])
-        l_overall.append(['Total', total_case, total_death])
-        self.save2File(l_overall, fname)
-        return l_overall
     ## save to csv
     ###def get_download_path(self):
         """Returns the default downloads path for linux or windows"""
-        """if os.name == 'nt':
-            import winreg
-            sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-            downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
-            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-                location = winreg.QueryValueEx(key, downloads_guid)[0]
-            return location
-        else:
-            return os.path.join(os.path.expanduser('~'), 'Downloads')"""
 
-    def downloadAndParseLink(self,link_address,fRaw,o_Raw):
+
+    def downloadAndParseLink(self,link_address,fRaw,o_Raw,d_path):
+        print(d_path)
         chrome_options1 = webdriver.ChromeOptions()
-        prefs = {'download.default_directory': 'C:\Dennis\Covid19\covid19viz\\nh\data_raw'}
+        prefs = {'download.default_directory': d_path}
         chrome_options1.add_experimental_option('prefs', prefs)
         siteOpen = webdriver.Chrome(chrome_options=chrome_options1)
 
@@ -112,7 +79,7 @@ class dataGrabNh(object):
         time.sleep(4)
         linkclick = siteOpen.find_element_by_css_selector(".suppressClickBusting")
         linkclick.click()
-        time.sleep(4)
+        time.sleep(5)
         try:
             os.rename(o_Raw, fRaw)
         except WindowsError:
@@ -120,80 +87,92 @@ class dataGrabNh(object):
             os.rename(o_Raw, fRaw)
         siteOpen.close()
 
-        i = 0
-        j=0
-        im = Image.open(fRaw).convert('L')
-        while i < 2:
-            while j < 2:
-                left = 265 + (200 * i)
-                right = 310 + (190 * i)
-                top = 300 + (150 * j)
-                bottom = 450 + (150 * j)
-                im1 = im.crop((left, top, right, bottom))
-                im1 = im1.resize((2 * (right-left),2 * (bottom-top)),Image.ANTIALIAS)
-                im1 = im1.resize((4 * (right-left), 4 * (bottom-top)), Image.ANTIALIAS)
-                im1 = im1.resize((6 * (right - left), 6 * (bottom - top)), Image.ANTIALIAS)
-                enhancer = ImageEnhance.Contrast(im1)
-                enhancer.enhance(2000000)
-                im1.show()
-                im1.save("C:/Dennis/Covid19/covid19viz/nh/data_raw/img" + str(i+j+1) + ".png", "PNG")
-
-                pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
-                text = pytesseract.image_to_string(im1,  config='-c tessedit_char_whitelist=0123456789').encode('utf8')
-                with open("C:/Dennis/Covid19/covid19viz/nh/data_raw/out" + str(i+j+1) + ".txt", 'w') as f:
-                    f.write(text)
-                    f.close()
-                j = j+1
-            i = i + 1
+        datas = self.readDataFromPng(fRaw)
 
         siteOpen.quit()  # close the window
-        return os.getcwd() + fRaw[1:]
+        return datas
     ## read data
     def readDataFromPng(self, f_namea):
         print('  B.readDataFromPng', f_namea)
         # step B: parse and open
         #---------------------------case-------------------------
         img = cv2.imread(f_namea)
-        delay = 0
-        while(delay < 10):
-            crop_img = img[300:620, 0:560]  # from 1080x650
-            cv2.imshow("readDataFromPng", crop_img)
-            key = cv2.waitKeyEx(1000)
-            if(key == 27 or key == 1048603):
-                break
-            delay += 1
- 
         custom_config = r'--oem 3 --psm 6'
-        text = pytesseract.image_to_string(crop_img, config=custom_config)
- 
-        print("  readDataFromPng:",text) 
-        return []
+        if os.name == 'nt':
+            pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
+        text = ''
+        crop_img = img[300:620, 260:560]
+        crop_img = cv2.resize(crop_img, (0, 0), fx=5, fy=5)
+        #filename = 'C:/Dennis/Covid19/covid19viz/nh/data_raw/savedImage.jpg'
+        #cv2.imwrite(filename, crop_img)
+        cv2.imshow("readDataFromPng", crop_img)
+        key = cv2.waitKeyEx(1000)
+        text = pytesseract.image_to_string(crop_img, config=custom_config).encode('utf8')
+        #print("  readDataFromPng:",text)
+        return text.splitlines()
 
     ## download a website
-    def saveData(self, fRaw, sRaw, oRaw):
+    def saveData(self, fRaw, sRaw, oRaw, dRaw):
+        print(dRaw)
         page_url = self.l_state_config[5][1]
         print('  download4Website ...', page_url)
-        if(not isfile(fRaw) ):
-            fRaw = self.downloadAndParseLink(page_url,fRaw, oRaw)
-        #scan and detect text	
-        fRaw = './nh/data_raw/nh_covid19_20200830.png'
-        data_info = self.readDataFromPng(fRaw)	
+        alldata = self.downloadAndParseLink(page_url,fRaw, oRaw, dRaw)
+        del alldata[5:8]
 
-        if(len(data_info) < 1): return []
-        data_csv = self.saveLatestDate(data_info, sRaw)
-        
-        return data_csv
+        allList = []
+        countyList = ['Belknap','Carroll','Cheshire','Coos','Grafton','Hillsborough','Merrimack','Rockingham','Strafford','Sullivan','Unknown','Total']
+        allList.append(['County','Cases', 'Deaths'])
+        i=0
+        for d in alldata:
+            d1 = d.split(' ')
+
+            try:
+                while d1.index("|") != -1:
+                    d1.remove("|")
+            except:
+                print("1")
+            print(d1)
+
+            j = 0
+            for d2 in d1:
+                #common replacement
+                d2 = d2.replace(',','')
+                d1[j] = d2
+                j = j + 1
+                print(d2)
+
+            dLength = len(d1)
+            print(dLength)
+            try:
+                allList.append([countyList[i],d1[dLength-6],d1[dLength-2]])
+            except:
+                print("1")
+            i = i + 1
+
+            print(d1)
+
+        with open(sRaw, 'wb') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            for c in allList:
+                wr.writerow(c)
+            myfile.close()
+
+
+        return allList
+
+
+    
 
     ## paser data CA
     def parseData(self, name_target, date_target, type_download):
             self.name_file = name_target
             f_name = self.state_dir + 'data_raw/' + self.state_name.lower() + '_covid19_' + self.name_file + '.png'
-            d_path = 'C:/Dennis/Covid19/covid19viz/nh/data_raw'
-            o_name = d_path + "/Map of Cumulative Positive Cases.png"
+            d_path = os.path.abspath("nh\\data_raw\\")
+            o_name = d_path + "\Map of Cumulative Positive Cases.png"
             s_name = self.state_dir + 'data/' + self.state_name.lower() + '_covid19_' + self.name_file + '.csv'
             if(not os.path.isdir(self.state_dir + 'data_raw/') ): os.mkdir(self.state_dir + 'data_raw/')
             # step A: downlowd and save
-            data_csv = self.saveData(f_name, s_name, o_name)
+            data_csv = self.saveData(f_name, s_name, o_name, d_path)
             print('  total list of cases', len(data_csv))
             return(data_csv, self.name_file, self.now_date)
 
--- nh/yo.py
+++
@@ -1,7 +0,0 @@
-from pdfminer.high_level import extract_text
-
-
-fData = extract_text("C:/Dennis/Covid19/covid19viz/nh/data_raw/nh_covid19_20200827.pdf")
-with open("C:/Dennis/Covid19/covid19viz/nh/bill827.txt", 'wb') as f:
-    f.write(fData)
-    f.close()
