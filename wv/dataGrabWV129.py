c06e7c7380b357f2039ace07734e7793284a361b
luna <lunawyh@gmail.com>
2020-11-02 16:51:49 -0500

    up load changed wv coad

--- wv/dataGrabWV129.py
+++ wv/dataGrabWV129.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-# 			dataGrabwv.py
+# 			dataGrabFl.py
 #
 #	grab data from FL state websites
 #
-#inspect
+#
 
 # ==============================================================================
 # -- imports -------------------------------------------------------------------
@@ -16,19 +16,32 @@ import pandas as pd
 import csv
 import datetime 
 import urllib
+import ssl
+import PyPDF2
 import re
 import requests
 from lxml import html
-import json
 import numpy as np
-from selenium import webdriver  # https://selenium-python.readthedocs.io/installation.html
+from PIL import Image
+import webbrowser
+import cv2
+import imgkit
+import os
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
 import time
-from selenium.webdriver.common.keys import Keys 
-import bs4
-from urllib2 import urlopen as uReq
-import numpy
-from datetime import date 
-from datetime import timedelta 
+from PIL import Image
+import autopy
+import pyautogui
+import subprocess
+import webbrowser as vb
+
+import tempfile
+import urlparse
+
+#from gi.repository import Poppler, Gtk
+
 
 
 # ==============================================================================
@@ -47,13 +60,6 @@ class dataGrabWV(object):
         self.l_state_config = l_config
         self.name_file = ''
         self.now_date = ''
-
-    ## save downloaded data to daily or overal data 
-    def saveLatestDateUt(self, l_raw_data):
-        #l_overall = []
-        self.save2File(l_raw_data, self.state_dir + 'data/'+self.state_name.lower()+'_covid19_'+self.name_file+'.csv')
-        print ('GHJJ')
-        return l_raw_data
     ## save to csv 
     def save2File(self, l_data, csv_name):
         csv_data_f = open(csv_name, 'w')
@@ -65,7 +71,23 @@ class dataGrabWV(object):
         for a_row in l_data:
             csvwriter.writerow(a_row)
         csv_data_f.close()
-        print('  save2File', csv_name)
+    ## parse from exel format to list 
+    def parseDfData(self, df, fName=None):
+        (n_rows, n_columns) = df.shape 
+        # check shape
+        #print('parseDfData', df.title)
+        lst_data = []
+        for ii in range(n_rows):
+            a_case = []
+            for jj in range(n_columns):
+                if( str(df.iloc[ii, jj]) == 'nan'  ): 
+                    a_case.append( 0 )
+                    continue
+                a_case.append( df.iloc[ii, jj] )
+            lst_data.append( a_case )
+        # save to a database file
+        if(fName is not None): self.save2File( lst_data, fName )
+        return lst_data
     ## open a csv 
     def open4File(self, csv_name):
         if(isfile(csv_name) ):
@@ -75,68 +97,84 @@ class dataGrabWV(object):
         return l_data
 
     ## download a website 
-    def getUpdatedDate(self, page_content, fRaw):
-        c_tree = html.fromstring(page_content)
-        print('    look for updated date')
-        se_dates = c_tree.xpath('//strong/text()')
-        for se_data in se_dates:
-            #print('  se_data', se_data)
-            if('/' in se_data):
-                print('      updated date', se_data)
-                # update file name
-                #print('      updated date from web', se_data)
-                se_data=se_data.split(' ')[0]  # .replace('1 p.m.', '').replace(' ', '')
-                print('      updated date', se_data)
-                dt_obj = datetime.datetime.strptime(se_data, '%m/%d/%y')
-                self.name_file = dt_obj.strftime('%Y%m%d')
-                self.now_date = dt_obj.strftime('%m/%d/%Y')
-                f_name = self.state_dir + 'data_html/'+self.state_name.lower()+'_covid19_'+self.name_file+'.html'
-                return f_name, se_dates
-        return fRaw, se_dates
-    ## download a website 
-    def saveWebsite(self, fRaw):
+    def download4Website(self, csv_url, fRaw):
         #csv_url = self.l_state_config[5][1]
+        # save csv file
+        urllib.urlretrieve(csv_url, fRaw)
+        return True
+    ## open a website 
+    def open4Website(self, fRaw):
+        csv_url = self.l_state_config[5][1]
+        print('  search website', csv_url)
+        # save html file
+        #urllib.urlretrieve(csv_url, fRaw)
+        # save html file
+        c_page = requests.get(csv_url)
+        c_tree = html.fromstring(c_page.content)
+        l_dates = c_tree.xpath('//a')  # ('//div[@class="col-xs-12 button--wrap"]')
+        print('   dddd', l_dates)
+        a_address = ''
+        for l_date in l_dates:
+            #print(l_date.text_content())
+            if('COVID-19 Daily Update') in l_date.text_content(): 
+                print('   sss', l_date)
+                a_address =l_date.get('href')
+                print('  find pdf at', a_address)
 
-        today = date.today()
-        print("Today is: ", today) 
-        datall = ''
-        # Yesterday date 
-        yesterday = today - timedelta(days = 1) 
-        print("Yesterday was: ", yesterday) 
+        print('11111111111111', a_address)
 
-        dt_obj = str(yesterday) 
-        print("++++++++++++++ ", dt_obj) 
-        print("++++++++++++++ ", type(dt_obj))
 
-        dt_obj = datetime.datetime.strptime(dt_obj, '%Y-%m-%d')
-        datall = dt_obj.strftime('%m-%d-%Y')
-        print("============ ", datall)
-        #"https://dhhr.wv.gov/News/2020/Pages/COVID-19-Daily-Update-10-28-2020.aspx"
-        csv_url2 = "https://dhhr.wv.gov/News/2020/Pages/COVID-19-Daily-Update-"+ datall + ".aspx"
-        print('----------- ', csv_url2)
+        return a_address
+
+
+
+    ## paser data FL
+    def dataDownload(self, name_target):
+            print('  A.dataDownload', name_target)
+            f_namea = self.state_dir + 'data_raw/'+self.state_name.lower()+'_covid19_'+name_target+'.aspx'
+            if(not os.path.isdir(self.state_dir + 'data_raw/') ): os.mkdir(self.state_dir + 'data_raw/')
+            # step A: downlowd and save
+            if( True): 
+                a_address = self.open4Website(f_namea)
+
+                if(a_address == ''): 
+                    print ('    No address of downloading PDF is found')
+                    return ('')
+
+  
+            return a_address
+    ## look for page containing confirmed data
+    def lookForConfirmedPage(self, pdfReader):
+        for page in range(12, 13):
+            pageObj = pdfReader.getPage(page)
+            pageTxt = pageObj.extractText()
+            # locate page
+            n_start = pageTxt.find('Cases no longer')
+            if(n_start >= 0): print('  found at page ', page) 
+            else: continue
+            return pageTxt
+        return ''
+    ## paser data FL
+    def dataReadConfirmed(self, f_name):
+        # save html file
 
         siteOpen = webdriver.Chrome()
-        siteOpen.get(csv_url2)
+        siteOpen.get(f_name)
         time.sleep(7)
-
-        # save html file
-        c_page = requests.get(csv_url2)
+        c_page = requests.get(f_name)
         c_tree = html.fromstring(c_page.content)
-        with open(fRaw, 'wb') as f:
-            f.write(c_page.content)
-        print('  saved to ', fRaw)
+      
 
         caseNumbers = siteOpen.find_elements_by_xpath('//font[@size="3"]')
 
-        #print('++++++++++', caseNumbers)
         case_num_list = []
         for case_num in caseNumbers:  # this is cases------------------------------------bc-bar-inner dw-rect
             dStringList = case_num.text.split()
-            #print('  ------------case_num', dStringList )
+            print('  ------------case_num', dStringList )
             case_num_list.append(dStringList)
         #print('1111111111111', case_num_list[10])
 
-        l_cases2 = np.reshape(case_num_list[10][3:], (len(case_num_list[10][3:])/2, 2)).T
+        l_cases2 = np.reshape(case_num_list[7][1:], (len(case_num_list[7][1:])/2, 2)).T
         print('22222222222222222', l_cases2)
         cases= []
         for c_c in l_cases2[1]:
@@ -158,24 +196,23 @@ class dataGrabWV(object):
             death += int(a_da[2])
         l_cases3 = np.append(l_data, [['Total', case, death]], axis=0)
         print('dddddddddddddddddd', l_cases3)
-        siteOpen.close()
-        return l_cases3
-
-    
-    ## paser data Ut
-    def parseData(self, name_file, date_target, type_download):
-            self.name_file = name_file
-            f_name = self.state_dir + 'data/'+self.state_name.lower()+'_covid19_'+self.name_file+'.csv'
-            if(not os.path.isdir(self.state_dir + 'data_html/') ): os.mkdir(self.state_dir + 'data_html/')
-
-            # step A: downlowd and save
-            lst_raw_data = self.saveWebsite(f_name)
-            #print('2222222222', lst_raw_data)
-
-            # step B: parse and open
-            lst_data = self.saveLatestDateUt(lst_raw_data)
-            return(lst_data, self.name_file, self.now_date)  #add in l_d_all
 
 
+        l_datas= []
+        return (l_datas)
 
+    ## paser data FL
+    def parseData(self, name_target, date_target, type_download):
+            self.name_file = name_target
+            self.now_date = date_target
+            #Step A download and save as raw PDF files
+            f_targeta = self.dataDownload(name_target)
+            if(f_targeta == ''): return ([], name_target, '')
+            #Step B read confirmed cases
+            l_d_sort = self.dataReadConfirmed(f_targeta)
+            #Step C read death cases
+        
 
+            return(l_d_sort, self.name_file, self.now_date)  
+
+## end of file
